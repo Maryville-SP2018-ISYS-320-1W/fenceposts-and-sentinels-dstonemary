2dab4c742c3f2994695a0d14029ece6be708b3e8
dstonemary <37548808+dstonemary@users.noreply.github.com>
2018-04-28 22:03:38 +0530

    Week 6 fenceposts & Sentinels

--- src/P1_FindMinAndMax.java
+++ src/P1_FindMinAndMax.java
@@ -10,15 +10,24 @@ public class P1_FindMinAndMax {
 
 	public static void main(String[] args) {
         Scanner console = new Scanner( System.in );
-        
+        System.out.print("Type a number (or -1 to stop): ");
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
-        
-        // YOUR CODE STARTS HERE
-
-
-        // YOUR CODE ENDS HERE
-        
+        int temp = 0;
+        while(console.hasNextInt()){
+        	temp = console.nextInt();
+        	if(temp == -1)
+        		break;
+        	else{
+        		if(temp >= max)
+        			max = temp;
+        		else
+        			if(temp <= min)
+        				min = temp;
+        		 System.out.print("Type a number (or -1 to stop): ");
+        	}
+        		
+        }
         System.out.println( "Maximum was " + max );
         System.out.println( "Minimum was " + min );
     }
--- src/P2_InfiniteGuessingGame.java
+++ src/P2_InfiniteGuessingGame.java
@@ -9,27 +9,65 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class P2_InfiniteGuessingGame {
-
+	
 	public static void main(String[] args) {
+		int wins = 0;
+		int loss = 0;
 		int secretNumber = getSecretNumber();
 		int numberOfGuessesAllowed = 7;
-
+		//System.out.println("secretNumber : "+secretNumber);
 		outputWelcomeMessage();
-		int guessesTaken = playGame(secretNumber, numberOfGuessesAllowed);
-		outputEndingMessage(guessesTaken, numberOfGuessesAllowed, secretNumber);
+		
+		
+		boolean isContinue = true;
+		String feedBack = "";
+		Scanner input = new Scanner(System.in);
+		int totalGuessesTaken = 1;
+		int totalGame = 0;
+		int sumOfGuess = 0;
+		int totalWin = 0 ;
+		int totalLoss = 0;
+		do{
+			int guessesTaken = playGame(secretNumber, numberOfGuessesAllowed);
+			boolean hasWon = outputEndingMessage(guessesTaken, numberOfGuessesAllowed, secretNumber);
+			totalGuessesTaken = totalGuessesTaken + guessesTaken;
+			totalGame ++;
+			if(hasWon)
+				totalWin = totalLoss + 1;
+			else
+				totalLoss = totalLoss + 1;
+			System.out.println("!!!!! For exit please enter 'quit'  !!!!!!!!");			
+			if(input.next().equalsIgnoreCase("quit")){
+				isContinue = false;
+				System.out.print("Please give your feedback the guessing game is too large or too small : ");
+				feedBack = input.nextLine();				
+			}
+			
+		}while(isContinue);
+		System.out.println("\nThe secret number was " + secretNumber + ".");
+		System.out.println(" feedBack about the guess game is "+feedBack);
+		System.out.println("The secret number was " + secretNumber + ".");
+		System.out.println("Total guessed token : "+totalGuessesTaken);
+		System.out.println("Total number of game played by you : "+totalGame);
+		System.out.println("Total number of wins : "+totalGame);
+		System.out.println("Total number of losses : "+totalLoss);
+		
+		
 	}
 
 	public static void outputWelcomeMessage() {
-		System.out.println("This is a guessing game.");
+		System.out.println("This is a guessing game. Each game will give you seven chances. You can continue the same game until you find the guessed number or you can quit by enter 'quit' at end of the game. ");
 		System.out.println("I picked a random number from 1 to 100");
 	}
 
-	public static void outputEndingMessage(int guessesTaken, int numberOfGuessesAllowed, int secretNumber) {
+	public static boolean outputEndingMessage(int guessesTaken, int numberOfGuessesAllowed, int secretNumber) {
+		boolean flag = false;
 		if (guessesTaken < numberOfGuessesAllowed) {
+			flag = true;
 			System.out.println("Congratuations, you guessed it!");
-		} else {
-			System.out.println("The secret number was " + secretNumber + ". Try again.");
-		}
+		} 
+		return flag;
+		
 	}
 
 	public static int getSecretNumber() {
@@ -39,13 +77,18 @@ public class P2_InfiniteGuessingGame {
 
 	public static int getNextGuess() {
 		Scanner console = new Scanner(System.in);
-		int nextGuess;
+		int nextGuess = 0;
 		do {
 			System.out.println("What is your guess? ");
-			nextGuess = console.nextInt();
-
-			if (nextGuess < 0 || nextGuess > 100) {
-				System.out.println("ERROR: Guess should be between 0 and 100");
+			if(console.hasNextInt()){
+				nextGuess = console.nextInt();
+	
+				if (nextGuess < 0 || nextGuess > 100) {
+					System.out.println("ERROR: Guess should be between 0 and 100");
+				}
+			} else {
+				if(console.next().equals("quit"))
+					break;
 			}
 		} while (nextGuess < 0 || nextGuess > 100);
 
